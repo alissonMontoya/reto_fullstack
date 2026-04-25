b9024e1d12fbed16a67da209c9fb7dded70879e3
alissonMontoya <alisson.montoya@upb.edu.co>
2026-04-25 06:08:46 -0500

    fix images github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   plugins: [react()],
-  base: "/reto_fullstack/"
+  base: "/reto_fullstack/",
 })
