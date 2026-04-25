ae2d66e1efe0b4f9c91971acc9874891e140a443
alissonMontoya <alisson.montoya@upb.edu.co>
2026-04-25 06:46:58 -0500

    cierre del proyecto

--- src/data/products.js
+++ src/data/products.js
@@ -11,7 +11,28 @@ const products = [
   { id: 9, name: "Tablet", price: 800000, image: `${import.meta.env.BASE_URL}imagenes/tablet.jpg` },
   { id: 10, name: "Smartwatch", price: 300000, image: `${import.meta.env.BASE_URL}imagenes/Smartwatch.jpg` },
   { id: 11, name: "Cámara", price: 950000, image: `${import.meta.env.BASE_URL}imagenes/camara.jpg` },
-  { id: 12, name: "Parlante", price: 200000, image: `${import.meta.env.BASE_URL}imagenes/parlante.jpg` }
+  { id: 12, name: "Parlante", price: 200000, image: `${import.meta.env.BASE_URL}imagenes/parlante.jpg` },
+
+  { id: 13, name: "Laptop", price: 2500000, image: `${import.meta.env.BASE_URL}imagenes/Laptop.jpg` },
+  { id: 14, name: "Disco SSD", price: 300000, image: `${import.meta.env.BASE_URL}imagenes/discossd.jpg` },
+  { id: 15, name: "Memoria USB", price: 40000, image: `${import.meta.env.BASE_URL}imagenes/memoriausb.jpg` },
+  { id: 16, name: "Router WiFi", price: 180000, image: `${import.meta.env.BASE_URL}imagenes/routerwifi.jpg` },
+  { id: 17, name: "Micrófono", price: 220000, image: `${import.meta.env.BASE_URL}imagenes/microfono.jpg` },
+  { id: 18, name: "Webcam", price: 160000, image: `${import.meta.env.BASE_URL}imagenes/webcam.jpg` },
+
+  { id: 19, name: "Trípode", price: 40000, image: `${import.meta.env.BASE_URL}imagenes/tripode.jpg` },
+  { id: 20, name: "Batería inalámbrica", price: 110000, image: `${import.meta.env.BASE_URL}imagenes/bateriainalambrica.jpg` },
+  { id: 21, name: "Lámpara", price: 30000, image: `${import.meta.env.BASE_URL}imagenes/lampara.jpg` },
+  { id: 22, name: "Mesa", price: 70000, image: `${import.meta.env.BASE_URL}imagenes/mesa.jpg` },
+  { id: 23, name: "Luces LED", price: 45000, image: `${import.meta.env.BASE_URL}imagenes/Luzled.jpg` },
+  { id: 24, name: "Espejo", price: 50000, image: `${import.meta.env.BASE_URL}imagenes/espejo.jpg` },
+
+  { id: 25, name: "Control Gamer", price: 200000, image: `${import.meta.env.BASE_URL}imagenes/controlgamer.jpg` },
+  { id: 26, name: "Bolso", price: 60000, image: `${import.meta.env.BASE_URL}imagenes/bolso.jpg` },
+  { id: 27, name: "Gafas Realidad Virtual", price: 500000, image: `${import.meta.env.BASE_URL}imagenes/gafasrv.jpg` },
+  { id: 28, name: "Impresora", price: 450000, image: `${import.meta.env.BASE_URL}imagenes/impresora.jpg` },
+  { id: 29, name: "Escáner", price: 300000, image: `${import.meta.env.BASE_URL}imagenes/escaner.jpg` },
+  { id: 30, name: "Proyector", price: 800000, image: `${import.meta.env.BASE_URL}imagenes/proyector.jpg` }
 ]
 
 export default products
