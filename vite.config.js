dd6350a831acb6af10416ed984734d589e3dc10a
alissonMontoya <alisson.montoya@upb.edu.co>
2026-04-25 01:50:46 -0500

    limpieza completa del proyecto y fix build

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    react({
-      jsxRuntime: 'automatic'
-    })
-  ],
-  build: {
-    sourcemap: true
-  }
+  plugins: [react()],
+  base: "/reto_fullstack/"
 })
