90d67aed092984b5c9118d7c29dc7548fcfc5ce5
alissonMontoya <alisson.montoya@upb.edu.co>
2026-04-25 06:12:56 -0500

    fix image paths for github pages

--- src/data/products.js
+++ src/data/products.js
@@ -18,21 +18,7 @@ const products = [
   { id: 15, name: "Memoria USB", price: 40000, image: "/reto_fullstack/imagenes/memoriausb.jpg" },
   { id: 16, name: "Router WiFi", price: 180000, image: "/reto_fullstack/imagenes/routerwifi.jpg" },
   { id: 17, name: "Micrófono", price: 220000, image: "/reto_fullstack/imagenes/microfono.jpg" },
-  { id: 18, name: "Webcam", price: 160000, image: "/reto_fullstack/imagenes/webcam.jpg" },
-
-  { id: 19, name: "Trípode", price: 40000, image: "/reto_fullstack/imagenes/tripode.jpg" },
-  { id: 20, name: "Batería inalámbrica", price: 110000, image: "/reto_fullstack/imagenes/bateriainalambrica.jpg" },
-  { id: 21, name: "Lámpara", price: 30000, image: "/reto_fullstack/imagenes/lampara.jpg" },
-  { id: 22, name: "Mesa", price: 70000, image: "/reto_fullstack/imagenes/mesa.jpg" },
-  { id: 23, name: "Luces LED", price: 45000, image: "/reto_fullstack/imagenes/Luzled.jpg" },
-  { id: 24, name: "Espejo", price: 50000, image: "/reto_fullstack/imagenes/espejo.jpg" },
-
-  { id: 25, name: "Control Gamer", price: 200000, image: "/reto_fullstack/imagenes/controlgamer.jpg" },
-  { id: 26, name: "Bolso", price: 60000, image: "/reto_fullstack/imagenes/bolso.jpg" },
-  { id: 27, name: "Gafas Realidad Virtual", price: 500000, image: "/reto_fullstack/imagenes/gafasrv.jpg" },
-  { id: 28, name: "Impresora", price: 450000, image: "/reto_fullstack/imagenes/impresora.jpg" },
-  { id: 29, name: "Escáner", price: 300000, image: "/reto_fullstack/imagenes/escaner.jpg" },
-  { id: 30, name: "Proyector", price: 800000, image: "/reto_fullstack/imagenes/proyector.jpg" },
+  { id: 18, name: "Webcam", price: 160000, image: "/reto_fullstack/imagenes/webcam.jpg" }
 ]
 
 export default products
